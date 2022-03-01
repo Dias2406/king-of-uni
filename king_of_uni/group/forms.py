4c505aeb5d88af1f484cfef0f9b601df951f598d
Dias2406 <75443246+Dias2406@users.noreply.github.com>
2022-03-01 23:38:45 +0000

    Change

--- king_of_uni/group/forms.py
+++ king_of_uni/group/forms.py
@@ -8,8 +8,5 @@ class CreateGroupForm(forms.ModelForm):
         model = Group
         fields = ['name']
 
-class JoinGroupForm(forms.ModelForm):
 
-    class Meta:
-        model = Account
-        fields = ['belongs_to_group']
+        
