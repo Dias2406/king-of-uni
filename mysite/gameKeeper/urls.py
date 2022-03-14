d1faaab2279ba6e7c67589c9b776a6385b9658f9
Dias2406 <75443246+Dias2406@users.noreply.github.com>
2022-03-14 17:11:39 +0000

    Moved create teams to Game Keeper views

--- mysite/gameKeeper/urls.py
+++ mysite/gameKeeper/urls.py
@@ -3,12 +3,19 @@ from django.urls import path
 from gameKeeper.views import (
     create_building_view,
     detail_building_view,
-    game_keeper_view
+    game_keeper_view,
+)
+
+from group.views import (
+    groups_list_view,
+    create_group_view,
 )
 
 app_name = 'game_keeper'
 
 urlpatterns = [
+    path('create_group/', create_group_view, name="create_group"),
+    path('group/', groups_list_view, name="groups_list"),
     path('settings/', game_keeper_view, name="settings"),
     path('create/', create_building_view, name="create"),
     path('<slug>/', detail_building_view, name="detail"),
